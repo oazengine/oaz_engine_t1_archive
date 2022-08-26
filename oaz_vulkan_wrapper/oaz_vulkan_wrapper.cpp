0ba924db093633ded907dae909303146636023e9
Sangmin Kim <noma@intereal.io>
2022-08-27 03:15:15 +0900

    Random: add new projects

---
+++ oaz_vulkan_wrapper/oaz_vulkan_wrapper.cpp
@@ -0,0 +1,10 @@
+// oaz_vulkan_wrapper.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnoazvulkanwrapper()
+{
+}
